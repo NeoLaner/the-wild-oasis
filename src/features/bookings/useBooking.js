9b470d31459236bb27329c4a6c387518254ffe9d
Yasin Askarian <ylanerofficial@gmail.com>
2023-08-21 16:18:29 +0330

    pagination

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -6,19 +6,26 @@ export function useBooking() {
   const [searchParams] = useSearchParams();
   const status = searchParams.get("status");
 
+  //1) Filter
   const filter =
-    status === undefined || status === "all"
-      ? null
-      : { field: "status", value: status };
+    !status || status === "all" ? null : { field: "status", value: status };
+
+  //2) Sort
+  const sortByRow = searchParams.get("sort") || "startDate-desc";
+  const [field, value] = sortByRow.split("-");
+  const sortBy = { field, value };
+
+  //3) Pagination
+  const curPage = searchParams.get("page") || 1;
 
   const {
-    data: bookings,
+    data: { data: bookings, count } = {},
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: getBookings.bind(this, filter),
+    queryKey: ["bookings", filter, sortBy, curPage],
+    queryFn: getBookings.bind(this, filter, sortBy, curPage),
   });
 
-  return { bookings, isLoading, error };
+  return { bookings, count, isLoading, error };
 }
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,4 +1,7 @@
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { RESULT_PER_PAGE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -55,3 +58,51 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Pagination({ count }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const totalPages = Math.ceil(count / RESULT_PER_PAGE);
+
+  function handlePrevious() {
+    const page = currentPage === 1 ? currentPage : currentPage - 1;
+    searchParams.set("page", page);
+    setSearchParams(searchParams);
+  }
+
+  function handleNext() {
+    const page = currentPage === totalPages ? currentPage : currentPage + 1;
+    searchParams.set("page", page);
+    setSearchParams(searchParams);
+  }
+
+  if (totalPages === 1) return null;
+
+  return (
+    <StyledPagination>
+      <P>
+        Showing <span>{(currentPage - 1) * RESULT_PER_PAGE + 1}</span> to{" "}
+        <span>
+          {currentPage === totalPages ? count : currentPage * RESULT_PER_PAGE}
+        </span>{" "}
+        of <span>{count}</span> results
+      </P>
+      <Buttons>
+        <PaginationButton onClick={handlePrevious} disabled={currentPage === 1}>
+          <HiChevronLeft />
+          <span>Previous</span>
+        </PaginationButton>
+
+        <PaginationButton
+          onClick={handleNext}
+          disabled={currentPage === totalPages}
+        >
+          <span>Next</span>
+          <HiChevronRight />
+        </PaginationButton>
+      </Buttons>
+    </StyledPagination>
+  );
+}
+
+export default Pagination;
