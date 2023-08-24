178ab78fdad087c1028edd5354443fd8aa3f386b
Neo <neo.laner.official@gmail.com>
2023-08-24 22:06:53 +0330

    add logout

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,13 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logOut as logOutAPi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 export function useLogout() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   const { mutate: logOut, isLoading } = useMutation({
     mutationFn: logOutAPi,
     onSuccess: () => {
       navigate("/login", { replace: true });
+      queryClient.removeQueries();
     },
   });
 
